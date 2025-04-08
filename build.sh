f2221287215cb51f0d1a63d8ebac9bf38b2e4d9d
sergeyvita <s.shaglin@gmail.com>
2025-04-08 13:11:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ rm -rf ~/.cache/pip
 echo ">>> Удаление openai (на всякий случай)..."
 pip uninstall -y openai
 
-echo ">>> Установка правильной версии OpenAI..."
-pip install --no-cache-dir openai==1.25.0
+echo ">>> Установка новой версии OpenAI..."
+pip install --no-cache-dir "openai>=1.25.1"
 
 echo ">>> Установка остальных зависимостей..."
 pip install --no-cache-dir flask==2.3.3 tiktoken
